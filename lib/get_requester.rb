ec0e865cb6588cd3e75f02b70d10b2f459a42265
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-02 00:45:06 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,7 +7,7 @@ class GetRequester
   
    URL ='https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
   
- def self.get_response_body
+ def get_response_body
   uri = URI.parse(URL)
   response = Net::HTTP.get_response(uri)
   response.body 
