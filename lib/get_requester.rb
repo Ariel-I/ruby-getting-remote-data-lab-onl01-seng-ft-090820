2b321531d788ca7c600a1e415a55afe8f81ec926
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-02 01:02:40 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -16,12 +16,13 @@ class GetRequester
  responses = GetRequester.new.get_response_body
  puts responses
  
-# def parse_json 
-#   parser = JSON.parse(self.get_parse)
-#   parser.collect do |parse|
-#     parse
-#   end 
-# end
+ def parse_json 
+   parser = JSON.parse(self.get_parse)
+   parser.collect do |parse|
+     parse
+   end 
+  get_response_body
+ end
   
   
   
